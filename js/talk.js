a031ab8605a4a722369168ac5d6ba2a4d05f762d
JoseAlderete11 <119827671+JoseAlderete11@users.noreply.github.com>
2023-10-12 11:03:29 -0500

    Add files via upload

---
+++ js/talk.js
@@ -0,0 +1,20 @@
+new TypeIt("#tqyxhj", {
+        loop: true,
+        cursorSpeed: 1000,
+        speed: 100
+    })
+    .type("JOSE & MARIA")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("MI GRAN AMOR ERES <br> TU😍❤️")
+    .pause(3000)
+    .go();
+
+new TypeIt('#talkToXHJ', {
+    lifeLike: true,
+    cursorSpeed: 1000,
+    waitUntilVisible: true,
+    speed: 100
+}).go();
